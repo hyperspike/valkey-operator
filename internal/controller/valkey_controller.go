59e477b892b4586358cbf13b41f16d99fc090e6a
Dan Molik <dan@danmolik.com>
2024-07-29 15:59:59 -0400

    keep on trucking

--- internal/controller/valkey_controller.go
+++ internal/controller/valkey_controller.go
@@ -24,7 +24,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	hyperspikeiov1 "hyperspike.io/valkey-operator/api/v1"
+	hyperv1 "hyperspike.io/valkey-operator/api/v1"
 )
 
 // ValkeyReconciler reconciles a Valkey object
@@ -49,28 +49,53 @@ type ValkeyReconciler struct {
 func (r *ValkeyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
+	valkey := &hyperv1.Valkey{}
+	if err := r.Get(ctx, req.NamespacedName, valkey); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if err := r.upsertConfigMap(ctx, valkey); err != nil {
+		return ctrl.Result{}, err
+	}
+	if err := r.upsertService(ctx, valkey); err != nil {
+		return ctrl.Result{}, err
+	}
+	if err := r.upsertServiceAccount(ctx, valkey); err != nil {
+		return ctrl.Result{}, err
+	}
+	if err := r.upsertSecret(ctx, valkey); err != nil {
+		return ctrl.Result{}, err
+	}
+	if err := r.upsertStatefulSet(ctx, valkey); err != nil {
+		return ctrl.Result{}, err
+	}
+
 	return ctrl.Result{}, nil
 }
 
-func (r *ValkeyReconciler) upsertConfigMap() {
-
+func (r *ValkeyReconciler) upsertConfigMap(ctx context.Context, valkey *hyperv1.Valkey) error {
+	return nil
 }
 
-func (r *ValkeyReconciler) upsertService() {
-
+func (r *ValkeyReconciler) upsertService(ctx context.Context, valkey *hyperv1.Valkey) error {
+	return nil
 }
 
-func (r *ValkeyReconciler) upsertServiceAccount() {
+func (r *ValkeyReconciler) upsertServiceAccount(ctx context.Context, valkey *hyperv1.Valkey) error {
+	return nil
 }
 
-func (r *ValkeyReconciler) upsertStatefulSet() {
+func (r *ValkeyReconciler) upsertStatefulSet(ctx context.Context, valkey *hyperv1.Valkey) error {
+	return nil
 }
 
-func (r *ValkeyReconciler) upsertSecret() {}
+func (r *ValkeyReconciler) upsertSecret(ctx context.Context, valkey *hyperv1.Valkey) error {
+	return nil
+}
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ValkeyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&hyperspikeiov1.Valkey{}).
+		For(&hyperv1.Valkey{}).
 		Complete(r)
 }
