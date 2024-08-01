34346039a6171d5c3c385259238c8b251c3d19a6
Dan Molik <dan@danmolik.com>
2024-08-01 07:51:56 -0400

    add ping command

--- go.mod
+++ go.mod
@@ -48,6 +48,7 @@ require (
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/valkey-io/valkey-go v1.0.43 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
--- internal/controller/valkey_controller.go
+++ internal/controller/valkey_controller.go
@@ -24,6 +24,9 @@ import (
 	"io"
 	"math/big"
 	"strings"
+	"time"
+
+	valkeyClient "github.com/valkey-io/valkey-go"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -111,11 +114,15 @@ func (r *ValkeyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 	if err := r.upsertServiceAccount(ctx, valkey); err != nil {
 		return ctrl.Result{}, err
 	}
-	if err := r.upsertSecret(ctx, valkey, true); err != nil {
+	password, err := r.upsertSecret(ctx, valkey, true)
+	if err != nil {
 		return ctrl.Result{}, err
 	}
 	if err := r.upsertStatefulSet(ctx, valkey); err != nil {
 		return ctrl.Result{}, err
+	}
+	if err := r.checkState(ctx, valkey, password); err != nil {
+		return ctrl.Result{Requeue: true, RequeueAfter: time.Second * 3}, nil
 	}
 
 	return ctrl.Result{}, nil
@@ -127,6 +134,27 @@ func labels(valkey *hyperv1.Valkey) map[string]string {
 		"app.kubernetes.io/instance":  valkey.Name,
 		"app.kubernetes.io/component": "valkey",
 	}
+}
+
+func (r *ValkeyReconciler) checkState(ctx context.Context, valkey *hyperv1.Valkey, password string) error {
+	logger := log.FromContext(ctx)
+
+	vClient, err := valkeyClient.NewClient(valkeyClient.ClientOption{InitAddress: []string{valkey.Name + "." + valkey.Namespace + ".svc:6379"}, Password: password})
+	if err != nil {
+		logger.Error(err, "failed to create valkey client", "valkey", valkey.Name, "namespace", valkey.Namespace)
+		return err
+	}
+	defer vClient.Close()
+	if err := vClient.Do(ctx, vClient.B().Ping().Build()).Error(); err != nil {
+		logger.Error(err, "failed to ping valkey", "valkey", valkey.Name, "namespace", valkey.Namespace)
+		return err
+	}
+	valkey.Status.Ready = true
+	if err := r.Client.Status().Update(ctx, valkey); err != nil {
+		logger.Error(err, "Valkey status update failed.", "valkey", valkey.Name, "namespace", valkey.Namespace)
+		return err
+	}
+	return nil
 }
 
 func (r *ValkeyReconciler) upsertService(ctx context.Context, valkey *hyperv1.Valkey) error {
@@ -272,13 +300,13 @@ func (r *ValkeyReconciler) upsertServiceHeadless(ctx context.Context, valkey *hy
 	return nil
 }
 
-func (r *ValkeyReconciler) upsertSecret(ctx context.Context, valkey *hyperv1.Valkey, once bool) error {
+func (r *ValkeyReconciler) upsertSecret(ctx context.Context, valkey *hyperv1.Valkey, once bool) (string, error) {
 	logger := log.FromContext(ctx)
 
 	logger.Info("upserting secret", "valkey", valkey.Name, "namespace", valkey.Namespace)
 	rs, err := randString(16)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	secret := &corev1.Secret{
@@ -292,26 +320,26 @@ func (r *ValkeyReconciler) upsertSecret(ctx context.Context, valkey *hyperv1.Val
 		},
 	}
 	if err := controllerutil.SetControllerReference(valkey, secret, r.Scheme); err != nil {
-		return err
+		return "", err
 	}
 	err = r.Get(ctx, types.NamespacedName{Namespace: valkey.Namespace, Name: valkey.Name}, secret)
 	if err != nil && errors.IsNotFound(err) {
 		if err := r.Create(ctx, secret); err != nil {
 			logger.Error(err, "failed to update secret", "valkey", valkey.Name, "namespace", valkey.Namespace)
-			return err
+			return "", err
 		}
 		r.Recorder.Event(valkey, "Normal", "Created",
 			fmt.Sprintf("Secret %s/%s is created", valkey.Namespace, valkey.Name))
 	} else if err == nil && !once {
 		if err := r.Update(ctx, secret); err != nil {
 			logger.Error(err, "failed to create secret", "valkey", valkey.Name, "namespace", valkey.Namespace)
-			return err
+			return "", err
 		}
 	} else {
 		logger.Error(err, "failed fetching secret", "valkey", valkey.Name, "namespace", valkey.Namespace)
-		return err
+		return "", err
 	}
-	return nil
+	return string(secret.Data["password"]), nil
 }
 
 func (r *ValkeyReconciler) upsertServiceAccount(ctx context.Context, valkey *hyperv1.Valkey) error {
